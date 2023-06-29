20b8f009e68a61b899a303e8e63144681574ea99
Alita-pixel <ninazhang255@gmail.com>
2023-06-29 22:06:20 +0300

    part new

--- part07-13_course_statistics/src/course_statistics.py
+++ part07-13_course_statistics/src/course_statistics.py
@@ -1,1 +1,47 @@
 # Write your solution here
+# import urllib.request
+# import json
+
+
+# my_request = urllib.request.urlopen("https://studies.cs.helsinki.fi/stats-mock/api/courses")
+# data = my_request.read().decode('utf-8')
+# courses = json.loads(data)
+# #print(courses)
+
+
+# def retrieve_all():
+#     final_course_list=[]
+#     course_list=[]
+#     for course in courses:
+#         #print(course["enabled"])输出许多行False\True
+#         if course["enabled"]=="True":
+#             course_list.append(course["fullName"],course["name"], course["year"],course["exercises"])
+#             print(course_list)
+#             # final_course_list=",".join(course_list)
+#             # print(final_course_list)
+
+import urllib.request
+import json
+
+def retrieve_all():
+    url = "https://studies.cs.helsinki.fi/stats-mock/api/courses"
+    my_request= urllib.request.urlopen(url)
+    
+    #type(my_request) #<class 'http.client.HTTPResponse'>
+    data=my_request.read()
+
+    courses = json.loads(data)
+
+    active_courses = []
+    for course in courses:
+        if course["enabled"] == "true":
+            active_courses.append(course["fullName"], course["name"], course["year"], sum(course["exercises"]))
+    
+    print(active_courses)
+
+retrieve_all()
+
+
+
+
+
--- part07-14_who_cheated/src/who_cheated.py
+++ part07-14_who_cheated/src/who_cheated.py
@@ -1,1 +1,13 @@
 # Write your solution here
+import csv
+
+def cheaters():
+    with open("start_times.csv") as my_file:
+        with open("submissions.csv") as another_file:
+            for line1 in csv.reader(my_file, delimiter=";"):
+                for line2 in csv.reader(another_file, delimiter=";"):
+                    if line2[0]==line1[0]:
+                        time=line2[1]-line1[1]
+                        print(line2[1],time)
+
+cheaters()
